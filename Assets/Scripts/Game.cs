4481d6839d54920f81d822a699fb47988b40da47
RuMonty <world.ru.monty@gmail.com>
2019-02-21 23:16:43 +0300

    setting the playing field

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Game : MonoBehaviour
 {
+    public static int GridHeight = 20;
+    public static int GridWeight = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,15 @@ public class Game : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public bool CheckIsInsideGrid(Vector2 position)
+    {
+        return ((int)position.x > 0 && (int)position.y > 0 && (int)position.x <= GridWeight);
+    }
+
+    public Vector2 Round(Vector2 position)
+    {
+        return new Vector2(Mathf.Round(position.x), Mathf.Round(position.y));
     }
 }
--- Assets/Scripts/TetroMino.cs
+++ Assets/Scripts/TetroMino.cs
@@ -2,17 +2,72 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class TetroMino : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool AllowRotation;
+    public bool LimitRotation;
+
+    float fall = 0;
+    public float fallspeed = 1;
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
-    {
-        
+    {        
+        CheckUserInput();
     }
+
+    void CheckUserInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
+            transform.position += new Vector3(-1, 0, 0);
+            if (!CheckIsVallidPosition())
+                transform.position += new Vector3(1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
+            transform.position += new Vector3(1, 0, 0);
+            if (!CheckIsVallidPosition())
+                transform.position += new Vector3(-1, 0, 0);
+
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) {
+            if (AllowRotation)
+            {
+                if (LimitRotation && transform.rotation.eulerAngles.z >= 90)
+                    transform.Rotate(0, 0, -90);
+                else
+                    transform.Rotate(0, 0, 90);
+                if (!CheckIsVallidPosition())
+                {
+                    if (LimitRotation && !(transform.rotation.eulerAngles.z >= 90))                    
+                            transform.Rotate(0, 0, 90);
+                    else
+                        transform.Rotate(0, 0, -90);
+                }
+            }
+        }
+        else if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) || Time.time - fall >= fallspeed) {
+            transform.position += new Vector3(0, -1, 0);
+            fall = Time.time;
+            if (!CheckIsVallidPosition())
+                transform.position += new Vector3(0, 1, 0);
+        }
+
+    }
+
+    bool CheckIsVallidPosition()
+    {
+        foreach (Transform mino in transform)
+        {
+            Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
+            if (!FindObjectOfType<Game>().CheckIsInsideGrid(pos))
+                return false;
+        }
+        return true;
+    }
+
 }
