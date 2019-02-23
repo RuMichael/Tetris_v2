8246062f6eaf1cf58f8ca86be7b5648e7ca00de1
RuMonty <world.ru.monty@gmail.com>
2019-02-23 20:02:36 +0300

    add comments and update grid

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -6,6 +6,7 @@ public class Game : MonoBehaviour
 {
     public static int GridHeight = 20;
     public static int GridWeight = 10;
+    public static Transform[,] grid = new Transform[GridWeight, GridHeight];
 
     // Start is called before the first frame update
     void Start()
@@ -19,23 +20,53 @@ public class Game : MonoBehaviour
         
     }
 
-    public void SpawnNextTetromino()
+    public void UpdateGrid(TetroMino tetroMino)         //запись в Grid новых Tetromino
     {
-        GameObject nextTetromino = (GameObject)Instantiate(Resources.Load(GetRandomTetromino(), typeof(GameObject)), new Vector2(5.0f, 20.0f), Quaternion.identity);     
+        for (int y = 0; y < GridHeight; y++)
+        {
+            for (int x = 0; x < GridWeight; x++)
+            {
+                if (grid[x, y] != null)
+                    if (grid[x, y].parent == tetroMino.transform)
+                        grid[x, y] = null;
+            }
+        }
+        foreach (Transform mino in tetroMino.transform)
+        {
+            Vector2 pos = Round(mino.position);
+
+            if (pos.y < GridHeight)
+                grid[(int)pos.x, (int)pos.y] = mino;
+
+        }
+
+    }
+
+    public Transform GetTransformAtGridPosition(Vector2 pos)  // возвращает значение из grid по значению Vector2
+    {
+        if (pos.y > GridHeight - 1)
+            return null;
+        else
+            return grid[(int)pos.x, (int)pos.y];
+    }
+
+    public void SpawnNextTetromino()        //спавн Tetromino
+    {
+        GameObject nextTetromino = (GameObject)Instantiate(Resources.Load(GetRandomTetromino(), typeof(GameObject)), new Vector2(4.0f, 20.0f), Quaternion.identity);     
         
     }
 
-    public bool CheckIsInsideGrid(Vector2 position)
+    public bool CheckIsInsideGrid(Vector2 position)     //проверка на превышение границ Grid
     {
-        return ((int)position.x >= 0 && (int)position.y > 0 && (int)position.x < GridWeight);
+        return ((int)position.x >= 0 && (int)position.y >= 0 && (int)position.x < GridWeight);
     }
 
-    public Vector2 Round(Vector2 position)
+    public Vector2 Round(Vector2 position)      //выравниваем до четных чисел Vector2
     {
         return new Vector2(Mathf.Round(position.x), Mathf.Round(position.y));
     }
 
-    string GetRandomTetromino()
+    string GetRandomTetromino()     //рандомный выбор tetromino для спавна 
     {
         string randomTetrominoName = "";
         int randomTetromino = Random.Range(1, 8);
--- Assets/Scripts/TetroMino.cs
+++ Assets/Scripts/TetroMino.cs
@@ -9,7 +9,7 @@ public class TetroMino : MonoBehaviour
     public bool LimitRotation;
 
     float fall = 0;
-    public float fallspeed = 1;
+    public float fallspeed = 1;  
 
     void Start()
     {
@@ -21,17 +21,22 @@ public class TetroMino : MonoBehaviour
         CheckUserInput();
     }
 
-    void CheckUserInput()
+    void CheckUserInput()  // работа с кнопками
     {
         if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) {
             transform.position += new Vector3(-1, 0, 0);
             if (!CheckIsVallidPosition())
                 transform.position += new Vector3(1, 0, 0);
+            else
+                FindObjectOfType<Game>().UpdateGrid(this);
+
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) {
             transform.position += new Vector3(1, 0, 0);
             if (!CheckIsVallidPosition())
                 transform.position += new Vector3(-1, 0, 0);
+            else
+                FindObjectOfType<Game>().UpdateGrid(this);
 
         }
         else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.Space)) {
@@ -43,11 +48,13 @@ public class TetroMino : MonoBehaviour
                     transform.Rotate(0, 0, 90);
                 if (!CheckIsVallidPosition())
                 {
-                    if (LimitRotation && !(transform.rotation.eulerAngles.z >= 90))                    
-                            transform.Rotate(0, 0, 90);
+                    if (LimitRotation && !(transform.rotation.eulerAngles.z >= 90))
+                        transform.Rotate(0, 0, 90);
                     else
                         transform.Rotate(0, 0, -90);
                 }
+                else
+                    FindObjectOfType<Game>().UpdateGrid(this);
             }
         }
         else if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) || Time.time - fall >= fallspeed) {
@@ -59,16 +66,20 @@ public class TetroMino : MonoBehaviour
                 enabled = false;
                 FindObjectOfType<Game>().SpawnNextTetromino();
             }
+            else
+                FindObjectOfType<Game>().UpdateGrid(this);
         }
 
     }
 
-    bool CheckIsVallidPosition()
+    bool CheckIsVallidPosition() //Проверяем возможность движения Tetromino
     {
         foreach (Transform mino in transform)
         {
             Vector2 pos = FindObjectOfType<Game>().Round(mino.position);
             if (!FindObjectOfType<Game>().CheckIsInsideGrid(pos))
+                return false;
+            if (FindObjectOfType<Game>().GetTransformAtGridPosition(pos) != null && FindObjectOfType<Game>().GetTransformAtGridPosition(pos).parent != transform)
                 return false;
         }
         return true;
